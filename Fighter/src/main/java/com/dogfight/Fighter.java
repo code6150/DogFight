a691e5c488b10f01c044b0be1590742e5472aeb3
code6150 <code6150@gmail.com>
2023-03-11 22:39:38 +0900

    update plugin

--- Fighter/src/main/java/com/dogfight/Fighter.java
+++ Fighter/src/main/java/com/dogfight/Fighter.java
@@ -5,8 +5,13 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class Fighter extends JavaPlugin {
 
     @Override
-    public void OnEnable() {
+    public void onEnable() {
+        System.out.println("개싸움 플러그인이 활성화 되었습니다!");
+    }
 
+    @Override
+    public void onDisable() {
+        System.out.println("개싸움 플러그인이 비활성화 되었습니다!");
     }
 
 }
